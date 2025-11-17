610dc9eda628c6f3486f5eecb0ba38a9c443caa0
Dziakula <ulyanadziakovich@gmail.com>
2025-11-17 11:35:18 +0100

    Favicon

--- .nuxt/dev/index.mjs
+++ .nuxt/dev/index.mjs
@@ -1037,7 +1037,7 @@ const _Eyu1uR5ASc8_zo4Kl45Ark_NuvUTYhdCOhvE0OBXtc = (function(nitro) {
 
 const rootDir = "/Users/ula/ustrzyki-gosciniec-strona";
 
-const appHead = {"meta":[{"name":"viewport","content":"width=device-width, initial-scale=1"},{"charset":"utf-8"}],"link":[],"style":[],"script":[],"noscript":[]};
+const appHead = {"meta":[{"name":"viewport","content":"width=device-width, initial-scale=1"},{"charset":"utf-8"}],"link":[{"rel":"icon","type":"image/png","sizes":"16x16","href":"/favicon-16x16.png?v=2"},{"rel":"icon","type":"image/png","sizes":"32x32","href":"/favicon-32x32.png?v=2"},{"rel":"icon","type":"image/png","sizes":"48x48","href":"/favicon-48x48.png?v=2"},{"rel":"icon","type":"image/png","sizes":"64x64","href":"/favicon-64x64.png?v=2"},{"rel":"apple-touch-icon","sizes":"180x180","href":"/apple-touch-icon.png?v=2"}],"style":[],"script":[],"noscript":[]};
 
 const appRootTag = "div";
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image']
+  modules: ['@nuxt/image'],
+
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png?v=2' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png?v=2' },
+        { rel: 'icon', type: 'image/png', sizes: '48x48', href: '/favicon-48x48.png?v=2' },
+        { rel: 'icon', type: 'image/png', sizes: '64x64', href: '/favicon-64x64.png?v=2' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png?v=2' }
+      ]
+    }
+  }
 })
